5bd4e32faec21e8798c96f232e6e141b8812541c
HusseinAli97 <venom.blackdesert@gmail.com>
2023-09-18 22:33:17 +0300

    change validation

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import logo from "../../images/logo.png";
 import { useFormik } from 'formik';
-import { Link,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import { Col, Button, Row, Container, Form, Modal, FloatingLabel } from 'react-bootstrap';
 import axios from 'axios';
@@ -24,7 +24,7 @@ export default function Register({ handleClose, show }) {
             toast.error(err.response.data.message);
             setIsLoading(false);
         });
-        if(data.message === "success") {
+        if (data.message === "success") {
             toast.success("Account Created Successfully");
             setIsLoading(false);
             navigate('/');
@@ -46,27 +46,32 @@ export default function Register({ handleClose, show }) {
         <>
             <ToastContainer position="top-right" autoClose={3000} hideProgressBar theme="colored" />
             <Modal show={show} centered onHide={handleClose} size='lg' className={`${styles.bg}`} >
-                <Container className={`p-0  shadow ${styles.bgCard}`}>
+                <Container className={`p-0  shadow ${styles.bgCard} position-relative`}>
                     <Row className='py-5'>
                         <Col xs={10} className={`mx-auto ${styles.bgForm}`}>
-                            <div className="d-flex flex-column w-100 justify-content-center align-items-center">
+                            <div className="d-flex flex-column w-100 justify-content-center align-items-center position-relative">
+                                <div className={`${styles.MnH} d-block  position-absolute top-0 start-0  `}>
+                                    {formik.errors.name && formik.touched.name ? <Form.Text className="textErorr fw-bold bg-dark px-2 py-1  rounded ms-auto d-flex bg-opacity-25">* {formik.errors.name}</Form.Text> : null}
+                                    {formik.errors.email && formik.touched.email ? <Form.Text className="textErorr fw-bold bg-dark px-2 py-1  rounded ms-auto d-block bg-opacity-25">* {formik.errors.email}</Form.Text> : null}
+                                    {formik.errors.password && formik.touched.password ? <Form.Text className="textErorr fw-bold bg-dark px-2 py-1 rounded ms-auto d-block bg-opacity-25">* {formik.errors.password}</Form.Text> : null}
+                                    {formik.errors.rePassword && formik.touched.rePassword ? <Form.Text className="textErorr fw-bold bg-dark  px-2 py-1 rounded ms-auto d-block bg-opacity-25">* {formik.errors.rePassword}</Form.Text> : null}
+                                    {formik.errors.phone && formik.touched.phone ? <Form.Text className="textErorr fw-bold bg-dark rounded px-2 py-1 ms-auto d-block bg-opacity-25">*{formik.errors.phone}</Form.Text> : null}
+                                </div>
                                 <div className="text-center">
                                     <img src={logo} width={"150px"} alt="logo" />
                                     <h4 className="mt-1 mb-2 pb-1">We are GLAM Team</h4>
                                 </div>
                                 <p>Please sign up to continue</p>
-                                <form className={`w-100   `} onSubmit={formik.handleSubmit}>
+                                <form className={`w-100     `} onSubmit={formik.handleSubmit}>
                                     <Row className='pb-2' >
                                         <Col md={6}>
                                             <FloatingLabel controlId="floatingName" label="Name" className="mb-2">
                                                 <Form.Control type="text" placeholder="Name" className='mb-2' name='name' value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                                                {formik.errors.name && formik.touched.name ? <Form.Text className="text-danger fw-bold bg-dark  rounded ms-auto d-flex bg-opacity-25">{formik.errors.name}</Form.Text> : null}
                                             </FloatingLabel>
                                         </Col>
                                         <Col md={6}>
                                             <FloatingLabel controlId="floatingEmail" label="Email address" className="mb-2">
                                                 <Form.Control type="email" placeholder="Email address" name='email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                                                {formik.errors.email && formik.touched.email ? <Form.Text className="text-danger fw-bold bg-dark  rounded ms-auto d-block bg-opacity-25">{formik.errors.email}</Form.Text> : null}
                                             </FloatingLabel>
                                         </Col>
                                     </Row>
@@ -74,19 +79,16 @@ export default function Register({ handleClose, show }) {
                                         <Col md={6}>
                                             <FloatingLabel controlId="floatingPassword" label="Password" className="mb-2">
                                                 <Form.Control type="password" placeholder="Password" name='password' value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                                                {formik.errors.password && formik.touched.password ? <Form.Text className="text-danger fw-bold bg-dark p-1 mt-2 rounded ms-auto d-block bg-opacity-25">{formik.errors.password}</Form.Text> : null}
                                             </FloatingLabel>
                                         </Col>
                                         <Col md={6}>
                                             <FloatingLabel controlId="floatingrePassword" label="Confirm Password" className="mb-2">
                                                 <Form.Control type="password" placeholder="Confirm Password" name='rePassword' value={formik.values.rePassword} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                                                {formik.errors.rePassword && formik.touched.rePassword ? <Form.Text className="text-danger fw-bold bg-dark p-1 mt-2 rounded ms-auto d-block bg-opacity-25">{formik.errors.rePassword}</Form.Text> : null}
                                             </FloatingLabel>
                                         </Col>
                                     </Row>
                                     <FloatingLabel controlId="floatingPhone" label="Phone Number" className="mb-2">
                                         <Form.Control type="tel" placeholder="Phone Number" name='phone' value={formik.values.phone} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                                        {formik.errors.phone && formik.touched.phone ? <Form.Text className="text-danger fw-bold bg-dark p-1 mt-2 rounded ms-auto d-block bg-opacity-25">{formik.errors.phone}</Form.Text> : null}
                                     </FloatingLabel>
                                     <div className="text-center py-1 mb-2 ">
                                         {isLoading ? <Button className="mb-2 w-100  " type="submit" variant="success">
