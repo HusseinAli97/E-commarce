7cf7c348f39362c6d672b6a5f287b0abea59f124
HusseinAli97 <venom.blackdesert@gmail.com>
2023-09-30 01:37:27 +0300

    Final BeforeChash

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,6 +1,36 @@
-import styles from './AboutUs.module.css'
+import React from "react";
+import styles from "./AboutUs.module.css"; // Import the updated CSS module
+import { Container } from "react-bootstrap";
+import BgHeader from "../bgHeader/bgHeader";
+
 export default function AboutUs() {
-    return (
-        <div>AboutUs</div>
-    )
+    return (<>
+        <BgHeader mainName="About Us" subName="Us" />
+        <Container fluid className={`d-flex align-items-start justify-content-center bg-white vh-100`}>
+            <div className={styles["about-container"]}>
+                <h2>About Us</h2>
+                <p className={styles["about-text"]}>
+                    Welcome to our online store! We are dedicated to providing you with the
+                    best products and services. Our team works tirelessly to ensure that
+                    you have a seamless shopping experience. Feel free to explore our
+                    diverse range of products and reach out to us if you have any questions
+                    or inquiries.
+                </p>
+                <div className={styles["contact-info"]}>
+                    <p>
+                        <i className={styles["info-icon"]} fas fa-map-marker-alt></i> Address: 1234 Elm
+                        Street, City, Country
+                    </p>
+                    <p>
+                        <i className={styles["info-icon"]} fas fa-envelope></i> Email:
+                        contact@example.com
+                    </p>
+                    <p>
+                        <i className={styles["info-icon"]} fas fa-phone></i> Phone: +1 (123) 456-7890
+                    </p>
+                </div>
+            </div>
+        </Container>
+    </>
+    );
 }
--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,6 +1,59 @@
-import styles from './ContactUs.module.css'
+import React, { useState } from "react";
+import styles from "./ContactUs.module.css"; // Import the updated CSS module
+import { Container } from "react-bootstrap";
+import BgHeader from "../bgHeader/bgHeader";
+
 export default function ContactUs() {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission here (e.g., sending data to a server)
+        console.log("Form data:", formData);
+    };
+
     return (
-        <div>ContactUs</div>
-    )
+        <>
+            <BgHeader mainName="Contact" subName="Us" />
+            <Container fluid className={`d-flex align-items-start justify-content-center bg-white vh-100`}>
+                <div className={styles["contact-container"]}>
+                    <h2>Contact Us</h2>
+                    <form className={styles["contact-form"]} onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Your Name"
+                            className={styles["form-input"]}
+                            onChange={handleChange}
+                        />
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Your Email"
+                            className={styles["form-input"]}
+                            onChange={handleChange}
+                        />
+                        <textarea
+                            name="message"
+                            placeholder="Your Message"
+                            className={styles["form-textarea"]}
+                            onChange={handleChange}
+                        ></textarea>
+                        <button type="submit" className={styles["submit-button"]}>
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </Container>
+        </>
+    );
 }
