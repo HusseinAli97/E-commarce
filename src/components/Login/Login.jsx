39eb5c7eb98c5181e7e19ecfa4852377e7020e21
HusseinAli97 <venom.blackdesert@gmail.com>
2023-10-03 15:53:54 +0300

    handel login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import { useFormik } from 'formik';
 import { useState, useContext } from 'react';
-import { Form, Col, FloatingLabel, Button, Row } from 'react-bootstrap';
+import { Form, Col, FloatingLabel, Button, Row, ToastContainer } from 'react-bootstrap';
 import { toast } from 'react-toastify';
 import { motion } from 'framer-motion';
 import axios from 'axios';
@@ -37,101 +37,97 @@ export default function Login({ onSuccessfulLogin }) {
                 // make API request to login
                 const response = await axios.post('https://route-ecommerce.onrender.com/api/v1/auth/signin', values);
                 //  successful login 
-                if (response.data.message === "success") {
-                    localStorage.setItem('userToken', response.data.token);
-                    setToken(response.data.token);
-                    onSuccessfulLogin();
-                    setIsLoading(false);
-                    toast.success("Login successful!", { position: "top-center", autoClose: 1500 });
-                }
+                localStorage.setItem('userToken', response.data.token);
+                setToken(response.data.token);
+                onSuccessfulLogin();
+                setIsLoading(false);
+                toast.success("Login successful!", { position: "top-center", autoClose: 1500 });
             } catch (error) {
                 //  login error
-                if (error.response) {
-                    setIsLoading(false);
-                    toast.error(error.response.data.message, { position: "top-center", autoClose: 1500 });
-                } else {
-                    setIsLoading(false);
-                    toast.error(error.message, { position: "top-center", autoClose: 3000 });
-                }
+                setIsLoading(false);
+                toast.error(error.response.data.message, { position: "top-center", autoClose: 1500 });
             }
         },
     });
     return (
-        <motion.div
-            key="login-form"
-            initial={{ opacity: 0, rotateY: -180 }}
-            animate={{ opacity: 1, rotateY: 0 }}
-            exit={{ opacity: 0, rotateY: 180 }}
-            transition={{ duration: 0.5 }}
-        >
-            <div className={` d-block position-absolute top-0 start-0 ${styles.MnH} `}>
-                {formik.errors.email && formik.touched.email && (
-                    <div className="textErorr fw-bold bg-dark px-2 py-1 rounded ms-auto d-block bg-opacity-25 mb-2">
-                        * {formik.errors.email}
-                    </div>
-                )}
-                {formik.errors.password && formik.touched.password && (
-                    <div className="textErorr fw-bold bg-dark px-2 py-1 rounded ms-auto d-block bg-opacity-25">
-                        * {formik.errors.password}
-                    </div>
-                )}
-            </div>
-            {
-                showForgotPassword ? <ForgetPassword backToLogin={handleSwitchForgotPassword} /> :
-                    <motion.div
-                        key="login-form"
-                        initial={{ opacity: 0, rotateY: -180 }}
-                        animate={{ opacity: 1, rotateY: 0 }}
-                        exit={{ opacity: 0, rotateY: 180 }}
-                        transition={{ duration: 0.5 }}
-                    >
-                        <Form onSubmit={formik.handleSubmit} autoComplete="off" >
-                            <Row className='pb-2 '>
-                                <Col md={12}>
-                                    <FloatingLabel controlId="floatingEmail" label="Email address" className="mb-2">
-                                        <Form.Control type="email"
-                                            placeholder="Email address"
-                                            name='email'
-                                            autoComplete='off'
-                                            value={formik.values.email}
-                                            onChange={formik.handleChange}
-                                            onBlur={formik.handleBlur} />
-                                    </FloatingLabel>
-                                </Col>
-                                <Col md={12}>
-                                    <FloatingLabel controlId="floatingPassword" label="Password" className="mb-2">
-                                        <Form.Control 
-                                        type="password" 
-                                        placeholder="Password" 
-                                        name='password' 
-                                        autoComplete='off' 
-                                        value={formik.values.password} 
-                                        onChange={formik.handleChange} 
-                                        onBlur={formik.handleBlur} />
-                                    </FloatingLabel>
-                                </Col>
-                            </Row>
-                            {isLoading ? <Button type="submit" className=' mb-2 px-5 mx-auto d-block' variant='primary'>
-                                <i className="fas fa-spinner fa-spin"></i>
-                            </Button>
-                                : <Button type="submit" disabled={!(formik.isValid && formik.dirty)} className='mb-2 px-5 mx-auto d-block' variant='primary'>Log in</Button>}
-                        </Form>
-                    </motion.div>
-            }
-            <div className={` d-flex align-items-center justify-content-center mt-3  ${styles.MnH} forgetPassword `}>
-                {!showForgotPassword ?
-                    <Button variant="link" onClick={handleSwitchForgotPassword} className=' link-light link-underline-opacity-25 link-underline-opacity-100-hover'>
-                        Forgot Password?
-                    </Button>
-                    :
-                    <Button variant="outline-primary" onClick={
-                        handleSwitchForgotPassword
-                    } className=' btn text-white position-absolute top-0 end-0'>
-                        Login <i className="fa-solid fa-arrow-right"></i>
-                    </Button>
+        <>
+        <ToastContainer />
+            <motion.div
+                key="login-form"
+                initial={{ opacity: 0, rotateY: -180 }}
+                animate={{ opacity: 1, rotateY: 0 }}
+                exit={{ opacity: 0, rotateY: 180 }}
+                transition={{ duration: 0.5 }}
+            >
+                <div className={` d-block position-absolute top-0 start-0 p-2 ${styles.MnH} `}>
+                    {formik.errors.email && formik.touched.email && (
+                        <div className="textErorr fw-bold bg-dark px-2 py-1 rounded ms-auto d-block bg-opacity-25 mb-2">
+                            * {formik.errors.email}
+                        </div>
+                    )}
+                    {formik.errors.password && formik.touched.password && (
+                        <div className="textErorr fw-bold bg-dark px-2 py-1 rounded ms-auto d-block bg-opacity-25">
+                            * {formik.errors.password}
+                        </div>
+                    )}
+                </div>
+                {
+                    showForgotPassword ? <ForgetPassword backToLogin={handleSwitchForgotPassword} /> :
+                        <motion.div
+                            key="login-form"
+                            initial={{ opacity: 0, rotateY: -180 }}
+                            animate={{ opacity: 1, rotateY: 0 }}
+                            exit={{ opacity: 0, rotateY: 180 }}
+                            transition={{ duration: 0.5 }}
+                        >
+                            <Form onSubmit={formik.handleSubmit} autoComplete="off" >
+                                <Row className='pb-2 '>
+                                    <Col md={12}>
+                                        <FloatingLabel controlId="floatingEmail" label="Email address" className="mb-2">
+                                            <Form.Control type="email"
+                                                placeholder="Email address"
+                                                name='email'
+                                                autoComplete='off'
+                                                value={formik.values.email}
+                                                onChange={formik.handleChange}
+                                                onBlur={formik.handleBlur} />
+                                        </FloatingLabel>
+                                    </Col>
+                                    <Col md={12}>
+                                        <FloatingLabel controlId="floatingPassword" label="Password" className="mb-2">
+                                            <Form.Control
+                                                type="password"
+                                                placeholder="Password"
+                                                name='password'
+                                                autoComplete='off'
+                                                value={formik.values.password}
+                                                onChange={formik.handleChange}
+                                                onBlur={formik.handleBlur} />
+                                        </FloatingLabel>
+                                    </Col>
+                                </Row>
+                                {isLoading ? <Button type="submit" className=' mb-2 px-5 mx-auto d-block' variant='primary'>
+                                    <i className="fas fa-spinner fa-spin"></i>
+                                </Button>
+                                    : <Button type="submit" disabled={!(formik.isValid && formik.dirty)} className='mb-2 px-5 mx-auto d-block' variant='primary'>Log in</Button>}
+                            </Form>
+                        </motion.div>
                 }
-            </div>
-        </motion.div>
+                <div className={` d-flex align-items-center justify-content-center mt-3  ${styles.MnH} forgetPassword `}>
+                    {!showForgotPassword ?
+                        <Button variant="link" onClick={handleSwitchForgotPassword} className=' link-light link-underline-opacity-25 link-underline-opacity-100-hover'>
+                            Forgot Password?
+                        </Button>
+                        :
+                        <Button variant="outline-primary" onClick={
+                            handleSwitchForgotPassword
+                        } className=' btn text-white position-absolute top-0 end-0'>
+                            Login <i className="fa-solid fa-arrow-right"></i>
+                        </Button>
+                    }
+                </div>
+            </motion.div>
+        </>
     );
 }
 
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import { useFormik } from 'formik';
 import { useState } from 'react';
 import { Form, Col, FloatingLabel, Button, Row } from 'react-bootstrap';
-import {  toast } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import { motion } from 'framer-motion';
 import axios from 'axios';
 import * as Yup from 'yup';
@@ -34,99 +34,95 @@ export default function SignUp({ switchToLoginForm }) {
                 setIsLoading(true);
                 const response = await axios.post('https://route-ecommerce.onrender.com/api/v1/auth/signup', values);
                 // successful registration 
-                if (response.data.message === "success") {
-                    setIsLoading(false);
-                    toast.success("Sign up successful!", { position: "top-center" });
-                    formik.resetForm();
-                    switchToLoginForm();
-                }
+                setIsLoading(false);
+                toast.success("Registration successful", { position: "top-center", autoClose: 1500 ,theme: 'colored'});
+                formik.resetForm();
+                switchToLoginForm();
             } catch (error) {
                 // registration error
-                if (error.response) {
-                    setIsLoading(false);
-                    toast.error(error.response.data.message, { position: "top-center" });
-                } else {
-                    setIsLoading(false);
-                    toast.error(error.message, { position: "top-center" });
-                }
+                setIsLoading(false);
+                toast.error(error.response.data.message, { position: "top-center", autoClose: 1500, theme: 'colored'});
             }
         },
     });
-    
+
     return (
-        <motion.div
-            key="register-form"
-            initial={{ opacity: 0, rotateY: -180 }}
-            animate={{ opacity: 1, rotateY: 0 }}
-            exit={{ opacity: 0, rotateY: 180 }}
-            transition={{ duration: 0.5 }}
-        >
-            {/* Errors Messages */}
-            <div className={` d-block position-absolute top-0 start-0 ${styles.MnH}`}>
-                {formik.errors.name && formik.touched.name && (
-                    <div className="textErorr fw-bold bg-dark px-2 py-1 rounded ms-auto d-flex bg-opacity-25 mb-2">
-                        * {formik.errors.name}
-                    </div>
-                )}
-                {formik.errors.email && formik.touched.email && (
-                    <div className="textErorr fw-bold bg-dark px-2 py-1 rounded ms-auto d-block bg-opacity-25 mb-2">
-                        * {formik.errors.email}
-                    </div>
-                )}
-                {formik.errors.password && formik.touched.password && (
-                    <div className="textErorr fw-bold bg-dark px-2 py-1 rounded ms-auto d-block bg-opacity-25 mb-2">
-                        * {formik.errors.password}
-                    </div>
-                )}
-                {formik.errors.rePassword && formik.touched.rePassword && (
-                    <div className="textErorr fw-bold bg-dark  px-2 py-1 rounded ms-auto d-block bg-opacity-25 mb-2">
-                        * {formik.errors.rePassword}
-                    </div>
-                )}
-                {formik.errors.phone && formik.touched.phone && (
-                    <div className="textErorr fw-bold bg-dark rounded px-2 py-1 ms-auto d-block bg-opacity-25 mb-2">
-                        * {formik.errors.phone}
-                    </div>
-                )}
-            </div>
+        <>
+            <ToastContainer />
+            <motion.div
+                key="register-form"
+                initial={{ opacity: 0, rotateY: -180 }}
+                animate={{ opacity: 1, rotateY: 0 }}
+                exit={{ opacity: 0, rotateY: 180 }}
+                transition={{ duration: 0.5 }}
+            >
+                {/* Errors Messages */}
+                <div className={` d-block position-absolute top-0 start-0 p-2 ${styles.MnH}`}>
+                    {formik.errors.name && formik.touched.name && (
+                        <div className="textErorr fw-bold bg-dark px-2 py-1 rounded ms-auto d-flex bg-opacity-25 mb-2">
+                            * {formik.errors.name}
+                        </div>
+                    )}
+                    {formik.errors.email && formik.touched.email && (
+                        <div className="textErorr fw-bold bg-dark px-2 py-1 rounded ms-auto d-block bg-opacity-25 mb-2">
+                            * {formik.errors.email}
+                        </div>
+                    )}
+                    {formik.errors.password && formik.touched.password && (
+                        <div className="textErorr fw-bold bg-dark px-2 py-1 rounded ms-auto d-block bg-opacity-25 mb-2">
+                            * {formik.errors.password}
+                        </div>
+                    )}
+                    {formik.errors.rePassword && formik.touched.rePassword && (
+                        <div className="textErorr fw-bold bg-dark  px-2 py-1 rounded ms-auto d-block bg-opacity-25 mb-2">
+                            * {formik.errors.rePassword}
+                        </div>
+                    )}
+                    {formik.errors.phone && formik.touched.phone && (
+                        <div className="textErorr fw-bold bg-dark rounded px-2 py-1 ms-auto d-block bg-opacity-25 mb-2">
+                            * {formik.errors.phone}
+                        </div>
+                    )}
+                </div>
 
-            <Form onSubmit={formik.handleSubmit}>
-                <Row className='pb-2'>
-                    <Col md={6}>
-                        <FloatingLabel controlId="floatingName" label="Name" className="mb-2">
-                            <Form.Control type="text" placeholder="Name" name='name' value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                        </FloatingLabel>
+                <Form onSubmit={formik.handleSubmit}>
+                    <Row className='pb-2'>
+                        <Col md={6}>
+                            <FloatingLabel controlId="floatingName" label="Name" className="mb-2">
+                                <Form.Control type="text" placeholder="Name" name='name' value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                            </FloatingLabel>
 
-                    </Col>
-                    <Col md={6}>
-                        <FloatingLabel controlId="floatingEmail" label="Email address" className="mb-2">
-                            <Form.Control type="email" placeholder="Email address" autoComplete='off'    name='email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                        </FloatingLabel>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={6}>
-                        <FloatingLabel controlId="floatingPassword" label="Password" className="mb-2">
-                            <Form.Control type="password" placeholder="Password" autoComplete='off'  name='password' value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                        </FloatingLabel>
-                    </Col>
-                    <Col md={6}>
-                        <FloatingLabel controlId="floatingrePassword" label="Confirm Password" className="mb-2">
-                            <Form.Control type="password" placeholder="Confirm Password" autoComplete='off' name='rePassword' value={formik.values.rePassword} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                        </FloatingLabel>
-                    </Col>
-                </Row>
-                <FloatingLabel controlId="floatingPhone" label="Phone Number" className="mb-2">
-                    <Form.Control type="tel" placeholder="Phone Number" name='phone' value={formik.values.phone} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                </FloatingLabel>
-                {isLoading ?
-                    <Button type="submit" className='my-3 px-5 mx-auto d-block' variant='success'>
-                        <i className="spinner-border spinner-border-sm"></i>
-                    </Button> :
-                    <Button type="submit" disabled={!(formik.isValid && formik.dirty)} className='my-3 px-5 mx-auto d-block' variant='success'>
-                        Sign up
-                    </Button>}
-            </Form>
-        </motion.div>
+                        </Col>
+                        <Col md={6}>
+                            <FloatingLabel controlId="floatingEmail" label="Email address" className="mb-2">
+                                <Form.Control type="email" placeholder="Email address" autoComplete='off' name='email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                            </FloatingLabel>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={6}>
+                            <FloatingLabel controlId="floatingPassword" label="Password" className="mb-2">
+                                <Form.Control type="password" placeholder="Password" autoComplete='off' name='password' value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                            </FloatingLabel>
+                        </Col>
+                        <Col md={6}>
+                            <FloatingLabel controlId="floatingrePassword" label="Confirm Password" className="mb-2">
+                                <Form.Control type="password" placeholder="Confirm Password" autoComplete='off' name='rePassword' value={formik.values.rePassword} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                            </FloatingLabel>
+                        </Col>
+                    </Row>
+                    <FloatingLabel controlId="floatingPhone" label="Phone Number" className="mb-2">
+                        <Form.Control type="tel" placeholder="Phone Number" name='phone' value={formik.values.phone} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                    </FloatingLabel>
+                    {isLoading ?
+                        <Button type="submit" className='my-3 px-5 mx-auto d-block' variant='success'>
+                            <i className="spinner-border spinner-border-sm"></i>
+                        </Button> :
+                        <Button type="submit" disabled={!(formik.isValid && formik.dirty)} className='my-3 px-5 mx-auto d-block' variant='success'>
+                            Sign up
+                        </Button>}
+                </Form>
+            </motion.div>
+        </>
     );
 }
